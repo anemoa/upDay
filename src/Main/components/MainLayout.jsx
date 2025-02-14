a5d90a364751ebfee02f1c494059a7dd3ca9e3b3
yu_hyeri <youhr02@gmail.com>
2025-02-14 16:57:31 +0900

    feat : 메인페이지 반응형 수정

--- src/Main/components/MainLayout.jsx
+++ src/Main/components/MainLayout.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 const MainLayout = ({ children }) => {
     return (
-        <main className='w-[80%] max-w-[1344px] mx-auto bg-blue-100 min-h-screen md:w-[70%]'>
+        <main className='w-[90%] md:w-[80%] md:max-w-[1344px] mx-auto flex flex-col md:flex-row gap-4 justify-between'>
             {children}
+            <section className='flex flex-col w-full md:w-[48%] gap-4 md:gap-6'></section>
         </main>
     );
 };
