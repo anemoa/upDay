a710ec60ad74da2e7f940bfa811bd0c2127653c8
Jun <19junlim@gmail.com>
2025-02-16 17:41:54 +0900

    fix: animation error fix

--- src/Intro/Intro.jsx
+++ src/Intro/Intro.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import Charac from './img/character-bulb.svg';
 import Arrow from './img/arrow-text.svg';
 import Bg from './img/bg-element.svg';
+import BgMobile from './img/BgMob2.svg'
 import List from './img/CardList.svg';
 import ListMobile from './img/CardListMobile.svg'
 import SearchBar from './components/SearchBar';
@@ -28,31 +29,41 @@ const Intro = () => {
             <Helmet>
                 <title>소개 페이지 - UpDay</title>
             </Helmet>
-            <div className='h-screen snap-start flex-grow flex-col items-center justify-center '>
-                <div className='flex w-[80%] max-w-[1344px] mx-auto justify-between items-center relative pt-[80px]'>
-                    <div className='fade-in flex flex-col items-start'>
+            <div className='h-screen snap-start flex-grow flex-col items-center justify-center'>
+                <div className='flex flex-col md:flex-row w-[80%] max-w-[1344px] mx-auto justify-between items-center relative pt-[80px]'>
+                    <div className='fade-in flex flex-col items-start w-full md:w-auto'>
                         <h1 className='text-5xl font-bold text-gray-800 leading-snug mb-4 animate-slide-up'>
                             좋은 습관, <br />
-                            매일 실천하고 싶으신가요?
+                            매일 실천하고 
+                            <br className='md:hidden'/>싶으신가요?
                         </h1>
                         <p className='text-3xl text-gray-600 mt-4 mb-8'>
                             작심삼일은 이제 그만! <br />
-                            <span className='text-4xl text-blue-600 font-bold'>
-                                Up Day
+                            <span className='text-4xl text-blue-500 font-bold'>
+                                UpDay
                             </span>
                             와 함께 <br className='md:hidden'/>
                             도전하고 성장하세요.
                         </p>
 
+                        {/* 이미지를 여기로 이동하고 모바일에서만 표시 */}
+                        <div className='w-full flex justify-end md:hidden mb-8'>
+                            <img
+                                src={Charac}
+                                alt='Character'
+                                className='w-auto h-[300px]'
+                            />
+                        </div>
+
                         {/* 반응형 버튼 크기 조정 */}
-                        <button className='btn btn-key w-full md:w-80 h-14 mt-10'
+                        <button className='btn btn-key w-full md:w-80 h-14'
                                 onClick={handleClick}>
                             성장하기
                         </button>
                     </div>
 
-                    {/* 반응형 이미지 숨기기 */}
-                    <div className='w-40% flex justify-center hidden md:block'>
+                    {/* 데스크톱에서만 이미지 표시 */}
+                    <div className='w-40% justify-center hidden md:flex'>
                         <img
                             src={Charac}
                             alt='Character'
@@ -61,7 +72,7 @@ const Intro = () => {
                     </div>
                 </div>
 
-                <div className='animate-bounce flex items-center justify-center mt-20'>
+                <div className='animate-bounce flex items-center justify-center mt-20 hidden md:flex'>
                     <div className='row-span-2 flex flex-col items-center'>
                         <p className='text-gray-600 text-sm'>성장하러 가기</p>
                         <img src={Arrow} alt='Arrow' className='mt-4' />
@@ -70,21 +81,26 @@ const Intro = () => {
             </div>
 
             <div className='h-screen snap-start flex-grow flex-col items-center justify-center'>
-                <div 
-                    className="fade-in w-[80%] max-w-[1344px] mx-auto relative z-10 text-center pt-[180px]"
-                >
+                <div className="fade-in w-[80%] max-w-[1344px] mx-auto relative z-10 text-center pt-[180px]">
+
                     <div className="relative w-full h-full">
+                        {/* 큰 화면에서는 object-contain */}
                         <img 
                             src={Bg} 
                             alt="Background" 
-                            className="absolute inset-0 w-full h-auto mx-auto object-contain max-h-[1000px] z-0 md:block hidden"
+                            className="absolute inset-0 w-full h-auto mx-auto object-contain max-h-[1000px] z-0 hidden md:block"
                         />
+                    </div>
+
+                    <div className='w-full flex justify-center md:hidden'>
+                        <img src={BgMobile} alt="Background" className='absolute top-0 w-auto h-[900px]' />
+
                     </div>
 
                     <FadeInSection>
                     <h1 className="text-xl md:text-2xl lg:text-3xl font-bold leading-[2] z-10 relative mt-10 md:mt-12 lg:mt-30 animate-slide-up">
-                    <span className='text-3xl text-blue-600 font-bold'>
-                                Up Day
+                    <span className='text-3xl text-blue-500 font-bold'>
+                                UpDay
                             </span>는 같은 목표를 <br className='md:hidden'/> 가진 사람들이
                     </h1>
 
@@ -111,7 +127,7 @@ const Intro = () => {
 
                     {/* 작동 되는 코드 웹, 모바일 둘다 잘됨 */}
                         <div className='h-screen snap-start flex-grow flex-col'>
-                            <div className='flex flex-col space-y-8 w-[80%] max-w-[1344px] mx-auto pt-40'>
+                            <div className='flex flex-col space-y-8 w-[70%] max-w-[1344px] mx-auto pt-10'>
                                 <FadeInSection delay={200}>
                                 <SpeechBubble text="👥 같은 목표를 가진 사람들과 응원하며 성장" position="left" />
                                 </FadeInSection>
@@ -119,14 +135,17 @@ const Intro = () => {
                                 <SpeechBubble text="🎯 매일 작은 실천으로 꾸준한 습관 형성" position="right" />
                                 </FadeInSection>
                                 <FadeInSection delay={600}>
-                                <SpeechBubble text="🔥 혼자가 아닌 함께, 더 즐겁고 쉽게 목표 완수!" position="left" />
+                                <SpeechBubble text="📈 함께라서 더 빠르고, 더 멀리 나아가는 여정!" position="left" />
+                                </FadeInSection>
+                                <FadeInSection delay={800}>
+                                <SpeechBubble text="🔥 혼자가 아닌 함께, 더 즐겁고 쉽게 목표 완수!" position="right" />
                                 </FadeInSection>
 
                                 <FadeInSection delay={800}>
-                                <p className='text-2xl text-center pt-35 leading-[2] font-bold pt-20'>
+                                <p className='text-2xl text-center pt-35 leading-[2] font-bold pt-10'>
                                     좋은 습관으로 매일 <br className='md:hidden'/>더 나은 나를 만들어보세요! <br />
-                                    오늘부터 <span className='text-3xl text-blue-600 font-bold'>
-                                            Up Day
+                                    오늘부터 <span className='text-3xl text-blue-500 font-bold'>
+                                            UpDay
                                         </span>와 <br className='md:hidden'/>함께 도전해볼까요? 💪😊
                                 </p>
                                 </FadeInSection>
@@ -140,7 +159,7 @@ const Intro = () => {
                     <p className='text-2xl font-bold text-center leading-relaxed animate-slide-up'>
                         현재 진행중인 챌린지
                     </p>
-                    <p className='text-5xl font-bold text-center leading-relaxed -mt-3 text-blue-600'>
+                    <p className='text-5xl font-bold text-center leading-relaxed -mt-3 text-blue-500'>
                         1,200개</p>
                 </FadeInSection>
                 <FadeInSection>
