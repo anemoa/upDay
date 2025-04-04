23536fcc5799dba7c219bace106b9b156ccddf96
anemoa <anemoia.yves@gmail.com>
2025-04-04 22:33:24 +0900

    refactor: unify MyPage component structure with project pattern

--- src/MyPage/MyPage.jsx
+++ src/MyPage/MyPage.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import MyPageLayout from './components/MyPageLayout';
+import { Helmet } from 'react-helmet';
+import { useDispatch } from 'react-redux';
+import { fetchMyPostFromSupabase } from '../store/features/userChallengeSlice';
 
 const MyPage = () => {
+
+	const dispacth = useDispatch();
+
+	useEffect(() => {
+		const userId = localStorage.getItem('loggedInUser');
+		dispacth(fetchMyPostFromSupabase(userId));
+	}, [dispacth]);
+
     return (
         <>
-            <MyPageLayout className="mt-[200px]"/>
+            <Helmet>
+                <title>마이페이지 - UpDay</title>
+            </Helmet>
+            <MyPageLayout className="mt-[200px]" />
         </>
     );
 };
--- src/MyPage/components/MyPageLayout.jsx
+++ src/MyPage/components/MyPageLayout.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState, useCallback } from 'react';
-import { Helmet } from 'react-helmet';
+import React from 'react';
 import UserProfile from './UserProfileSection';
 import UserReport from './UserReportSection';
 import TabSwitcher from './TabSwitcher';
@@ -7,9 +6,6 @@ import TabSwitcher from './TabSwitcher';
 const MyPageLayout = () => {
     return (
         <main className="w-[90%] md:w-[80%] md:max-w-[1344px] mx-auto flex flex-col md:flex-row gap-4 justify-between">
-            <Helmet>
-                <title>마이페이지 - UpDay</title>
-            </Helmet>
             <section className="flex flex-col w-full md:w-[48%] gap-4 md:gap-6">
                 <UserProfile />
                 <UserReport />
